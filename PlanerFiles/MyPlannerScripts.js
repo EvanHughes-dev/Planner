d027eb7658be982e8f670b9c4c0b4b2987df96cc
24ehughes <68885900+memelord2000880@users.noreply.github.com>
2022-11-09 09:50:55 -0500

    11/9 Update

--- PlanerFiles/MyPlannerScripts.js
+++ PlanerFiles/MyPlannerScripts.js
@@ -29,117 +29,105 @@ function GoRight() {
     ReturnDays(a);
  
 
-}
+}//end of GoRight
+function GoLeft() {
+    a--;
+    ReturnDays(a);
 
+
+}
+//document.getById("Due1").innerHTML = "x" + br + "y" + br + z 
+MonthsTest = []
+YearTest = []
+var IdForCalenderBackground = ['Day1Back', 'Day2Back', 'Day3Back', 'Day4Back', 'Day5Back', 'Day6Back', 'Day7Back', 'Day8Back', 'Day9Back', 'Day10Back', 'Day11Back', 'Day12Back', 'Day13Back', 'Day14Back', 'Day15Back', 'Day16Back', 'Day17Back', 'Day18Back', 'Day19Back', 'Day20Back', 'Day21Back', 'Day22Back', 'Day23Back', 'Day24Back', 'Day25Back', 'Day26Back', 'Day27Back', 'Day28Back', 'Day29Back', 'Day30Back', 'Day31Back', 'Day32Back', 'Day33Back',  'Day34Back', 'Day35Back', 'Day36Back', 'Day37Back', 'Day38Back', 'Day39Back', 'Day40Back', 'Day41Back', 'Day42Back', 'Day43Back', 'Day44Back' ]
+var IdForCalender = ['Day1', 'Day2', 'Day3', 'Day4', 'Day5', 'Day6', 'Day7', 'Day8', 'Day9', 'Day10', 'Day11', 'Day12', 'Day13', 'Day14', 'Day15', 'Day16', 'Day17', 'Day18', 'Day19', 'Day20', 'Day21', 'Day22', 'Day23', 'Day24', 'Day25', 'Day26', 'Day27', 'Day28', 'Day29', 'Day30', 'Day31', 'Day32', 'Day33', 'Day34', 'Day35', 'Day36', 'Day37', 'Day38', 'Day39', 'Day40', 'Day41', 'Day42']
+var IDForClanderText = []
 var DatesForCalender = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];//an array for all the cells
 var AllMonths = ["January", "Febuary", "March", "April", "May", "June", "July", "Agust", "September", "October", "November", "December"];
-function ReturnDays(a) {//starts the whole calender event and assigns the values
+var DueArray = ['Due1', 'Due2', 'Due3', 'Due4', 'Due5', 'Due6', 'Due7', 'Due8', 'Due9', 'Due10', 'Due11', 'Due12', 'Due13', 'Due14', 'Due15', 'Due16', 'Due17', 'Due18', 'Due19', 'Due20', 'Due21', 'Due22', 'Due23', 'Due24', 'Due25', 'Due26', 'Due27', 'Due28', 'Due29', 'Due30', 'Due31', 'Due32', 'Due33', 'Due34', 'Due35', 'Due36', 'Due37', 'Due38', 'Due39', 'Due40', 'Due41', 'Due42']
 
+function ReturnDays(a) {//starts the whole calender event and assigns the values
+    
     DatesForCalender = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     var b = 0
     var c = 0
     var date = new Date();
+    var day = date.getDate()
     var month = date.getMonth() + a;
     while(month >= 12) {
         b = (12 - month)*-1;
         month = b
         c++;
     }
-    var year = date.getFullYear() + c;
-    Day(date, month, year);
-    //asigns all the cells the correct number
-    document.getElementById('Day1').innerHTML = DatesForCalender[0];
-    if (DatesForCalender[0] > 0) {
-        document.getElementById('Day1').style.backgroundColor = "#808080";
+
+    while (month <= -1){
+        b = 12 + month
+        month = b
+        c--;
     }
-    document.getElementById('Day2').innerHTML = DatesForCalender[1];
-    document.getElementById('Day3').innerHTML = DatesForCalender[2];
-    document.getElementById('Day4').innerHTML = DatesForCalender[3];
-    document.getElementById('Day5').innerHTML = DatesForCalender[4];
-    document.getElementById('Day6').innerHTML = DatesForCalender[5];
-    document.getElementById('Day7').innerHTML = DatesForCalender[6];
-    document.getElementById('Day8').innerHTML = DatesForCalender[7];
-    document.getElementById('Day9').innerHTML = DatesForCalender[8];
-    document.getElementById('Day10').innerHTML = DatesForCalender[9];
-    document.getElementById('Day11').innerHTML = DatesForCalender[10];
-    document.getElementById('Day12').innerHTML = DatesForCalender[11];
-    document.getElementById('Day13').innerHTML = DatesForCalender[12];
-    document.getElementById('Day14').innerHTML = DatesForCalender[13];
-    document.getElementById('Day15').innerHTML = DatesForCalender[14];
-    document.getElementById('Day16').innerHTML = DatesForCalender[15];
-    document.getElementById('Day17').innerHTML = DatesForCalender[16];
-    document.getElementById('Day18').innerHTML = DatesForCalender[17];
-    document.getElementById('Day19').innerHTML = DatesForCalender[18];
-    document.getElementById('Day20').innerHTML = DatesForCalender[19];
-    document.getElementById('Day21').innerHTML = DatesForCalender[20];
-    document.getElementById('Day22').innerHTML = DatesForCalender[21];
-    document.getElementById('Day23').innerHTML = DatesForCalender[22];
-    document.getElementById('Day24').innerHTML = DatesForCalender[23];
-    document.getElementById('Day25').innerHTML = DatesForCalender[24];
-    document.getElementById('Day26').innerHTML = DatesForCalender[25];
-    document.getElementById('Day27').innerHTML = DatesForCalender[26];
-    document.getElementById('Day28').innerHTML = DatesForCalender[27];
-    document.getElementById('Day29').innerHTML = DatesForCalender[28];
-    document.getElementById('Day30').innerHTML = DatesForCalender[29];
-    document.getElementById('Day31').innerHTML = DatesForCalender[30];
-    document.getElementById('Day32').innerHTML = DatesForCalender[31];
-    document.getElementById('Day33').innerHTML = DatesForCalender[32];
-    document.getElementById('Day34').innerHTML = DatesForCalender[33];
-    document.getElementById('Day35').innerHTML = DatesForCalender[34];
-    document.getElementById('Day36').innerHTML = DatesForCalender[35];
-    document.getElementById('Day37').innerHTML = DatesForCalender[36];
-    document.getElementById('Day38').innerHTML = DatesForCalender[37];
-    document.getElementById('Day39').innerHTML = DatesForCalender[38];
-    document.getElementById('Day40').innerHTML = DatesForCalender[39];
-    document.getElementById('Day41').innerHTML = DatesForCalender[40];
-    document.getElementById('Day42').innerHTML = DatesForCalender[41];
+    var year = date.getFullYear() + c;
+    Day(day, month, year);
+
+    //asigns all the cells the correct number
+
+    for (i = 0; i < DatesForCalender.length; i++) { document.getElementById(IdForCalender[i]).innerHTML = DatesForCalender[i]; }
+    
     //assigns the month title
-    document.getElementById('Month').innerHTML = AllMonths[month];
+    document.getElementById('Month').innerHTML = AllMonths[month] + ": " + year;
 
 
-    ChangeColorChangeBefore(DatesForCalender[0], 'Day1')
-    ChangeColorChangeBefore(DatesForCalender[1], 'Day2')
-    ChangeColorChangeBefore(DatesForCalender[2], 'Day3')
-    ChangeColorChangeBefore(DatesForCalender[3], 'Day4')
-    ChangeColorChangeBefore(DatesForCalender[4], 'Day5')
-    ChangeColorChangeBefore(DatesForCalender[5], 'Day6')
-
-
-    ChangeColor(DatesForCalender[25], 'Day26')
-    ChangeColor(DatesForCalender[26], 'Day27')
-    ChangeColor(DatesForCalender[27], 'Day28')
-    ChangeColor(DatesForCalender[28], 'Day29')
-    ChangeColor(DatesForCalender[29], 'Day30')
-    ChangeColor(DatesForCalender[30], 'Day31')
-    ChangeColor(DatesForCalender[31], 'Day32')
-    ChangeColor(DatesForCalender[32], 'Day33')
-    ChangeColor(DatesForCalender[33], 'Day34')
-    ChangeColor(DatesForCalender[34], 'Day35')
-    ChangeColor(DatesForCalender[35], 'Day36')
-    ChangeColor(DatesForCalender[36], 'Day37')
-    ChangeColor(DatesForCalender[37], 'Day38')
-    ChangeColor(DatesForCalender[38], 'Day39')
-    ChangeColor(DatesForCalender[39], 'Day40')
-    ChangeColor(DatesForCalender[40], 'Day41')
-    ChangeColor(DatesForCalender[41], 'Day42')
+    
+    for (i = 0; i < DatesForCalender.length; i++) {
+      
+        ChangeColorChangeBefore(DatesForCalender[i], MonthsTest[i] ,IdForCalenderBackground[i], day, month,  a)
+    }
+    for (i = 0; i < DatesForCalender.length; i++) { document.getElementById(DueArray[i]).innerHTML = " "}
+    
+    for (i = 0; i < DatesForCalender.length; i++){
+       
+        run =0 
+        x = DatesForCalender[i]
+        y=MonthsTest[i]
+        z=YearTest[i]
+        SaveDay = JSON.parse(localStorage.getItem("DaySaved"))
+        SaveMonth = JSON.parse(localStorage.getItem("MonthSaved"))
+        SaveYear = JSON.parse(localStorage.getItem("YearSaved"))
+        
+     
+        //document.writeln(JSON.parse(localStorage.getItem("MonthSaved")));
+        
+        for (k = 0; k < SaveYear.length;k++) {
+            
+            if (x == SaveDay[k] && y == SaveMonth[k] && z == SaveYear[k]) {
+                SetText = JSON.parse(localStorage.getItem("titles"))
+                
+                if (run != 0) {document.getElementById(DueArray[i]).innerHTML =  document.getElementById(DueArray[i]).innerHTML +"</br>" + SetText[k]; }
+                else { document.getElementById(DueArray[i]).innerHTML = SetText[k]; run++;}
+            }
+             
+        }
+       
+        
+    }
 
 } //ReturnDays()
 
-function ChangeColorChangeBefore(Value, Day) {
-    if (Value > 15) {
-        document.getElementById(Day).style.backgroundColor = "#808080";
-    } else {
-        document.getElementById(Day).style.backgroundColor = "white";
+function ChangeColorChangeBefore(ValueDay, ValueMonth, Day, Current, month, a) {
+
+   
+    if (month != ValueMonth) {
+        document.getElementById(Day).style.backgroundColor = "#8C8984";
+    } else if (ValueDay == Current && month-a == ValueMonth) {
+        document.getElementById(Day).style.backgroundColor = "#0191C8";
+        //document.writeln(month, ValueMonth)
+    } 
+else{
+        document.getElementById(Day).style.backgroundColor = "#74C2E1";
     }
 }
 
-function ChangeColor(Value, Day) {
-    if (Value < 20) {
-        document.getElementById(Day).style.backgroundColor = "#808080";
-    } else {
-        document.getElementById(Day).style.backgroundColor = "white";
-    }
-}
+
+
 
 function daysInMonth(month, year) {//how many days are in a given month of a given year
     return new Date(year, month, 0).getDate();
@@ -149,30 +137,48 @@ function daysInMonth(month, year) {//how many days are in a given month of a giv
 }//daysInMonth(month, year)
 
 function Day(date, month, year) {  //function decides what day each cell of the 42 are
-    DayOfWeek = new Date(year, month, 1).getDay(); // gets days in current week
-    document.getElementById('test').innerHTML =month
+    var DayOfWeek = new Date(year, month, 1).getDay(); // gets days in current week
+    
     var DayTemp = 1;//assigns a temp day
     var DayTemp2 = 1;
+   // document.getElementById('test').innerHTML = MonthsTest
 
-
-
-    MonthBefore = new Date(year, month - 1, 0);//finds number of days in last month
+    
+    MonthBefore = new Date(year, month , 0);//finds number of days in last month
     
 
     for (i = 0; i < DatesForCalender.length;) {
         if (i <= DayOfWeek-1) {// if the number of times run is less then the position of the first day in the calender
-            DatesForCalender[i] = MonthBefore.getDate() - (DayOfWeek-i); // asign the first day that would be seen of the last month
-
+            var DateInWeek = 6 - DayOfWeek
+           
+            DatesForCalender[5-DateInWeek-i] = MonthBefore.getDate() - i; // asign the first day that would be seen of the last month
+            
+            if (month  >= 0 ) {
+                MonthsTest[i] = month - 1
+                YearTest[i] = year
+             
+            } else { MonthsTest[i] = 11; YearTest[i] = year - 1 }
+          
         } else if (i >= DayOfWeek && i - DayOfWeek <  daysInMonth(month+1, year)) { // if the number of times run falls in the current month
 
             DatesForCalender[i] = DayTemp//day temp adds by one 
             DayTemp++;
+            MonthsTest[i] = month 
+            YearTest[i] = year
         } else {
+
+            if (month <= 11) {
+                MonthsTest[i] = month + 1
+                YearTest[i] = year
+
+            } else { MonthsTest[i] = 0; YearTest[i]= year+1 }
+           
             DatesForCalender[i] = DayTemp2//days after the month ends
             DayTemp2++;
         }
 
         i++;
+    
     }//for i=0
 
 }// Day(date, month, year)
@@ -197,11 +203,11 @@ function closeNav() {//closes the sidebar
 
 //start of functions for form
 
-var DueDatesCheckedDay = [];
-var  DueDatesCheckedMonth = [];
-var DueDatesCheckedYear = [];
-var NameOfCheck = [0];
 
+
+var DueDatesCheckedDay = [0];
+var DueDatesCheckedMonth = [0];
+var DueDatesCheckedYear = [0];
 
 
 
@@ -209,55 +215,28 @@ var NameOfCheck = [0];
 
 
 function GetDataInput() {//gets the data for the user new event input
-    
+   
     
     var Title;
-    var DateDueDay = new Date(document.getElementById('Date').value).getDate();
+    var DateDueDay = new Date(document.getElementById('Date').value).getDate()+1;
    var  DateDueMonth = new Date(document.getElementById('Date').value).getMonth();
     var DateDueYear = new Date(document.getElementById('Date').value).getFullYear();
     Title = document.getElementById('Title').value;
-
-    
-    
-    if (localStorage.getItem("DaySaved") === null) {
-
-        
-
-
-
-
-    } else {
-        DueDatesCheckedDay = JSON.parse(localStorage.getItem("DaySaved"));
-        DueDatesCheckedMonth = JSON.parse(localStorage.getItem("MonthSaved"));
-        DueDatesCheckedYear = JSON.parse(localStorage.getItem("YearSaved"));
-    }
-
-    DueDatesCheckedDay.push(DateDueDay + 1);//these two are flipped because the form returns them weird
-    DueDatesCheckedMonth.push(DateDueMonth + 1);
-    DueDatesCheckedYear.push(DateDueYear);
-    NameOfCheck.push(Title);
-
-    localStorage.setItem("DaySaved", JSON.stringify(DueDatesCheckedDay));
-    localStorage.setItem("MonthSaved", JSON.stringify(DueDatesCheckedMonth));
-    localStorage.setItem("YearSaved", JSON.stringify(DueDatesCheckedYear));
-    
-    
+  // document.writeln(DateDueDay, DateDueMonth, DateDueYear)
     CreateCheckBox(Title);
-    document.getElementById('date').value = "";
+    StoreDate(DateDueDay , DateDueMonth, DateDueYear);
+   
+    
+   
+    document.getElementById('Date').value = "";
     document.getElementById('Title').value = "";
    
-  
+    
 
    
 }//GetDataInput()
 
-function FindLocation(){
-    var Location = NameOfCheck.indexOf('The')
-    
-   
-    
-   
-}
+
 
 function CreateCheckBox(Name){
    
@@ -268,21 +247,48 @@ function CreateCheckBox(Name){
        
     var TypedTitle = []
     if (JSON.parse(localStorage.getItem("titles")) !== null) { TypedTitle = JSON.parse(localStorage.getItem("titles")); TypedTitle.push(Name); } else {TypedTitle[0] = Name }
-        
+    
     
     
 
     localStorage.removeItem("titles")
 
-    document.writeln(TypedTitle)
+   
         localStorage.setItem("titles", JSON.stringify(TypedTitle));
-    
+   
     
 
     
 }
 
+//------------------------------------------\\
 
+//start functions for save date
+
+
+function StoreDate(Day, Month, Year) {
+    var TypedDay = []
+    var TypedMonth = []
+    var TypedYear = []
+   // document.writeln("Ran2")
+    if (JSON.parse(localStorage.getItem("DaySaved")) !== null) { TypedDay = JSON.parse(localStorage.getItem("DaySaved")); TypedDay.push(Day); } else {TypedDay[0]=Day }
+    if (JSON.parse(localStorage.getItem("MonthSaved")) !== null) { TypedMonth = JSON.parse(localStorage.getItem("MonthSaved")); TypedMonth.push(Month); } else {TypedMonth[0]=Month }
+    if (JSON.parse(localStorage.getItem("YearSaved")) !== null) { TypedYear = JSON.parse(localStorage.getItem("YearSaved")); TypedYear.push(Year); } else {TypedYear[0]=Year }
+    //document.writeln("Ran3")
+    
+
+    localStorage.removeItem("DaySaved")
+    localStorage.setItem("DaySaved", JSON.stringify(TypedDay));
+    localStorage.removeItem("MonthSaved")
+    localStorage.setItem("MonthSaved", JSON.stringify(TypedMonth));
+    localStorage.removeItem("YearSaved")
+    localStorage.setItem("YearSaved", JSON.stringify(TypedYear));
+    
+    Begin();
+   
+}
+
+//-------------------------------------------\\
 
 //begin support functions
 
@@ -301,7 +307,7 @@ function Refreshed() {
 
 */
 
-    
+    //document.writeln("Ran5")
 
     var Temp;
     Temp = JSON.parse(localStorage.getItem("titles"));
@@ -319,7 +325,7 @@ function Refreshed() {
                 li.appendChild(t);
                 li.setAttribute("id", Name)
                
-                
+               
                
              
 
@@ -338,12 +344,22 @@ function Refreshed() {
                     if (tempIndex > -1) {
                       
                         tempArray.splice(tempIndex, 1);
-                        
+                        TypedDay = JSON.parse(localStorage.getItem("DaySaved"))
+                        TypedMonth = JSON.parse(localStorage.getItem("MonthSaved"))
+                        TypedYear = JSON.parse(localStorage.getItem("YearSaved"))
+                        TypedDay.splice(tempIndex, 1)
+                        TypedMonth.splice(tempIndex, 1)
+                        TypedYear.splice(tempIndex, 1)
                     }
                     localStorage.setItem("titles", JSON.stringify(tempArray));
                     document.getElementById("MyList").removeChild(event.target);
-                    
-                });
+                    localStorage.removeItem("DaySaved")
+                    localStorage.setItem("DaySaved", JSON.stringify(TypedDay));
+                    localStorage.removeItem("MonthSaved")
+                    localStorage.setItem("MonthSaved", JSON.stringify(TypedMonth));
+                    localStorage.removeItem("YearSaved")
+                    localStorage.setItem("YearSaved", JSON.stringify(TypedYear));
+                });//this removes a item from the to do list
                 
 
 
@@ -355,5 +371,3 @@ function Refreshed() {
         }
     }
 }
-
-
