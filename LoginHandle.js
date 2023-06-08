03fc040b91e5381dfc752d310299469686a0bb6c
24ehughes <68885900+memelord2000880@users.noreply.github.com>
2023-06-08 19:18:36 +0000

    OK, this time

--- LoginHandle.js
+++ LoginHandle.js
@@ -1,1 +1,13 @@
+import jwt_decode from "jwt-decode";
+function onSignIn(authInfos) {
+    const responsePayload = decodeJwtResponse(authInfos);
+   // document.write("ID: " +responsePayload.sub);
+}
 
+//https://developers.google.com/identity/gsi/web/guides/handle-credential-responses-js-functions
+
+function decodeJwtResponse(token){
+var decoded = jwt_decode(token);
+
+console.log(decoded);
+}
