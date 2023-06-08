b0676452707ec9c0d216be1878b30d866d2e677c
24ehughes <68885900+memelord2000880@users.noreply.github.com>
2023-06-08 20:10:07 +0000

    I'm out of ideas

--- LoginHandle.js
+++ LoginHandle.js
@@ -14,3 +14,29 @@ var decoded = jwtDecode(token);
 
 console.log(decoded);
 }
+
+function jwtDecode(token, options) {
+    if (typeof token !== "string") {
+        throw new InvalidTokenError("Invalid token specified: must be a string");
+    }
+
+    options = options || {};
+    var pos = options.header === true ? 0 : 1;
+
+    var part = token.split(".")[pos];
+    if (typeof part !== "string") {
+        throw new InvalidTokenError("Invalid token specified: missing part #" + (pos + 1));
+    }
+
+    try {
+        var decoded = base64_url_decode(part);
+    } catch (e) {
+        throw new InvalidTokenError("Invalid token specified: invalid base64 for part #" + (pos + 1) + ' (' + e.message + ')');
+    }
+
+    try {
+        return JSON.parse(decoded);
+    } catch (e) {
+        throw new InvalidTokenError("Invalid token specified: invalid json for part #" + (pos + 1) + ' (' + e.message + ')');
+    }
+}
