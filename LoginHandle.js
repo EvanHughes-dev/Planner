293f19aef17b366488e95d3f1b79051e62daaa4c
24ehughes <68885900+memelord2000880@users.noreply.github.com>
2023-06-08 20:45:08 +0000

    f

--- LoginHandle.js
+++ LoginHandle.js
@@ -10,7 +10,12 @@ function onSignIn(authInfos){
 //https://developers.google.com/identity/gsi/web/guides/handle-credential-responses-js-functions
 
 function decodeJwtResponse(token){
-var decoded = jwtDecode(token);
-
-console.log(decoded);
+   
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    
+        return JSON.parse(jsonPayload);
 }
