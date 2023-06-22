e3fd483b425134772b7e689afd523f25a692f916
24ehughes <68885900+memelord2000880@users.noreply.github.com>
2023-06-22 20:20:48 +0000

    update

--- LoginHandle.js
+++ LoginHandle.js
@@ -1,8 +1,5 @@
 
-function onSignIn(authInfos){
-    document.writeln(authInfos.credential); 
 
-}
 
 
 //https://developers.google.com/identity/gsi/web/guides/handle-credential-responses-js-functions
