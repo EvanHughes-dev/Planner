3f29342435f4533c479668daea90bdcd8a394f27
24ehughes <68885900+memelord2000880@users.noreply.github.com>
2023-06-23 00:40:15 +0000

    see if this works

--- LoginHandle.js
+++ LoginHandle.js
@@ -1,5 +1,6 @@
-
-function CredentialHandle(credentials){
-    document.writeln(credentials); 
+//file for handeling login info
+function CredentialHandle(credentials){//gets credentials
+    //credentials not deciphered yet
+    
 }
     
