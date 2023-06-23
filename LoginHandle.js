ca0872b72e735310a3f16ecafff17935483d98e2
24ehughes <68885900+memelord2000880@users.noreply.github.com>
2023-06-23 10:46:20 +0000

    more cleanup

--- LoginHandle.js
+++ LoginHandle.js
@@ -10,33 +10,6 @@ var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
 //#region Decode a JWT token
 //source https://github.com/auth0/jwt-decode
-function polyfill(input) {
-    var str = String(input).replace(/=+$/, "");
-   
-    for (
-        // initialize result and counters
-        var bc = 0, bs, buffer, idx = 0, output = "";
-        // get next character
-        (buffer = str.charAt(idx++));
-        // character found in table? initialize bit storage and add its ascii value;
-        ~buffer &&
-        ((bs = bc % 4 ? bs * 64 + buffer : buffer),
-            // and if not first of each 4 characters,
-            // convert the first 8 bits to one ascii character
-            bc++ % 4) ?
-        (output += String.fromCharCode(255 & (bs >> ((-2 * bc) & 6)))) :
-        0
-    ) {
-        // try to find character in table (0-63, not found => -1)
-        buffer = chars.indexOf(buffer);
-    }
-    return output;
-}
-
-var atob = (typeof window !== "undefined" &&
-    window.atob &&
-    window.atob.bind(window)) ||
-polyfill;
 
 function b64DecodeUnicode(str) {
     return decodeURIComponent(
